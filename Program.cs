6ba8441d174f5ca132f42e65ed397ca7337a722c
desolami <99566510+desolami@users.noreply.github.com>
2026-03-11 02:52:57 +0100

    Add files via upload
    A simple C# console game where the user tries to guess a randomly generated number with feedback and input validation.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Random rand = new Random();
+int number = rand.Next(1, 11);
+int guess;
+
+Console.WriteLine("Guess a number between 1 and 10");
+while (!int.TryParse(Console.ReadLine(), out guess))
+{
+    Console.WriteLine("Invalid input. Please enter a number:");
+}
+if (guess > number)
+{
+    Console.WriteLine("Too high");
+}
+
+
+else if (guess < number)
+{
+    Console.WriteLine("Too low");
+}
+else
+{
+    Console.WriteLine("Congratulations! You guessed the number.");
+}
+Console.WriteLine("Do you want to play again? (yes/no)");
+    
